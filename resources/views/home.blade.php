e108f236cad5da0ec58002f01aef50f94068ab0f
varanustoken <varanustoken@gmail.com>
2023-04-26 13:27:33 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,10 +44,10 @@
                         <h1 class="text-uppercase">TORKGO CHAIN</h1>
                         <p>The next generation Blockchain ecosystem </p>
                         <a href="https://t.me/TorkGoChain" class="default-btn"><span class="fab fa-telegram"></span> <span>JOIN US</span></a>
-                        <a href="https://pancakeswap.finance/swap?outputCurrency=0xD174f90940F7fdB1C9C1BE4a53395e2E5E4f85D9" class="default-btn"><span class="fas fa-cart-plus"></span> <span>BUY TORKGO</span></a>
-                        <a href="https://poocoin.app/tokens/0xD174f90940F7fdB1C9C1BE4a53395e2E5E4f85D9" class="default-btn"><span class="fa fa-bar-chart"></span> <span>CHART</span></a>
+                        <a href="https://pancakeswap.finance/swap?outputCurrency=0x" class="default-btn"><span class="fas fa-cart-plus"></span> <span>BUY TORKGO</span></a>
+                        <a href="https://poocoin.app/tokens/0x" class="default-btn"><span class="fa fa-bar-chart"></span> <span>CHART</span></a>
                         <div class="row"></div>
-                        Contract Address : <b>0xD174f90940F7fdB1C9C1BE4a53395e2E5E4f85D9</b>
+                        Contract Address : <b>0x</b>
                     </div>
                 </div>
                 <div class="col-lg-6">
